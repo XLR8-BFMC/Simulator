ff892d3be9fd86f7d55c0ac3cc17ef31cdcc6fc7
andreiasnico <andreiasnico@gmail.com>
2025-02-17 18:44:02 +0200

    pipe communication demo in example/camera.py

--- src/example/src/camera.py
+++ src/example/src/camera.py
@@ -47,21 +47,31 @@ class CameraHandler():
         self.cv_image = np.zeros((1080 , 1920))
         rospy.init_node('CAMnod', anonymous=True)
         self.image_sub = rospy.Subscriber("/automobile/camera/camera_follow/image_raw", Image, self.callback)
-        
+        self.fifo = '/shared/capture_pipe'
+        if not os.path.exists(self.fifo):
+                os.mkfifo(self.fifo) 
         rospy.spin()
 
     def callback(self, data):
-        """
-        :param data: sensor_msg array containing the image in the Gazsbo format
-        :return: nothing but sets [cv_image] to the usefull image that can be use in opencv (numpy array)
-        """
-        self.cv_image = self.bridge.imgmsg_to_cv2(data, "rgb8")
-        ##os.chdir('/catkin_ws')
-        _, base = cv2.imencode(".jpg", self.cv_image)
-        pngAsText = base64.b64encode(base)
-        print(pngAsText[0])
-        ## TODO :  to be sent in pipe or anywhere somebody wants        
-    
+        try:
+            """
+            :param data: sensor_msg array containing the image in the Gazsbo format
+            :return: nothing but sets [cv_image] to the usefull image that can be use in opencv (numpy array)
+            """
+            self.cv_image = self.bridge.imgmsg_to_cv2(data, "rgb8")
+            _, encoded_image = cv2.imencode(".jpg", self.cv_image)
+            image_bytes = encoded_image.tobytes()
+            with open(self.fifo, "wb") as pipe:
+                pipe.write(len(image_bytes).to_bytes(4, byteorder="big"))  # Send image size
+                pipe.write(image_bytes)  # Send image data
+                pipe.flush()
+        except BrokenPipeError:
+            rospy.logwarn("Broken pipe! No reader available.")
+        except Exception as e:
+            rospy.logerr(f"Error processing image: {e}")
+            
+
+        
             
 if __name__ == '__main__':
     try:
